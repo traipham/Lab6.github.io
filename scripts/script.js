8c3c0ab2ee283f21e638aea47bb6a610620c8303
Trai Pham <traipham16@yahoo.com>
2021-05-06 19:59:27 -0700

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -16,10 +16,10 @@ document.addEventListener('DOMContentLoaded', () => {
    * the current entry for each journal-entry element.
    */
 
-  let url = ""  // SET URL 
+  let url = "https://cse110lab6.herokuapp.com/entries"  // SET URL
 
   fetch(url)
-    .then( /* FILL IN RESPONSE HANDLING HERE */ )
+    .then(response => response.json())
     .then(entries => {
       entries.forEach((entry) => {
        
@@ -27,8 +27,10 @@ document.addEventListener('DOMContentLoaded', () => {
 
 
         // CODE GOES HERE vvv
+        newPost = document.createElement('journal-entry');
+        newPost.entry = entry;
+        document.querySelector('main').appendChild(newPost);
         
-
 
 
 
